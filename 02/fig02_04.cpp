a6eecf6dd6bade0b40d58888b0fae8697a29c615
Martins <ildefonso.junior@gmail.com>
2026-04-07 04:46:10 -0300

    new file:   fig02_04.cpp

---
+++ 02/fig02_04.cpp
@@ -0,0 +1,11 @@
+// Figura 2.4: fig02_04.cpp
+// Imprimindo múltiplas linhas de texto com um única instrução.
+#include <iostream> // permite que o programa gere saída de dados na tela
+
+// a função main inicia a execução do programa
+int main()
+{
+    std::cout << "Welcome\nto\n\nC++!";
+
+    return 0; // indica que o programa terminou com sucesso
+} // fim da função main
