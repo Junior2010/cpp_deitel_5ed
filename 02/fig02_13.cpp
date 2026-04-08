986991d2bd543d42726123dc97fbcd439497c8d1
Martins <ildefonso.junior@gmail.com>
2026-04-08 07:00:56 -0300

    Iniciado o new file:   fig02_13.cpp

---
+++ 02/fig02_13.cpp
@@ -0,0 +1,3 @@
+// Figura 2.13: fig03_13.cpp
+// Comparando inteiros utilizando isntruções if, operandos relacionais
+// e operadores de igualdade
---
+++ 02/teste.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main()
+{
+	std::cout << 17.2 % 5.5;
+}
