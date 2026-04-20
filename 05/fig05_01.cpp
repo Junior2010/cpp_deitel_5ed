6cd4e48d774547a01121c820b4aff08ce771b098
Martins <ildefonso.junior@gmail.com>
2026-04-20 10:06:23 -0300

    fig04_21.cpp

---
+++ 05/fig05_01.cpp
@@ -0,0 +1,19 @@
+// Figura 5.1: fig05_01.cpp
+// Repetição controlada por contador.
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int counter = 1; // declara e inicializa a variável de controle
+
+    while ( counter <= 10 ) // condição de continuação do loop
+    {
+        cout << counter << " ";
+        counter++; // incrementa a variável de controle por 1
+    } // fim do while
+
+    cout << endl; // gera a saída de um caractere de nova linha
+    return 0; // terminação bem-sucedida
+} // fim de main
