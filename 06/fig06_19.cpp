a0423faa96e79ce52691647b52bdb88ab48049cb
martins <ildefonso.junior@gmail.com>
2026-05-08 09:39:37 -0300

    fig06_19.cpp

---
+++ 06/fig06_19.cpp
@@ -0,0 +1,13 @@
+// Figura 6.19: fig06_19.cpp
+// Comparando a passagem por valor e a passagem por referência com as referências.
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int squareByValue( int ); // protótipo de função (passagem por valor)
+int squareByReference( int & ); // protótipo de função (passagem por referência)
+
+int main()
+{
+    int
+}
