cfab4a69dc470e75f226b35dc9407935006be4dc
martins <ildefonso.junior@gmail.com>
2026-05-12 06:51:48 -0300

    fig06_19.cpp, fig06_20.cpp, fig06_21.cpp, fig06_22.cpp e fig06_23.cpp

---
+++ 06/fig06_21.cpp
@@ -0,0 +1,14 @@
+// Figura 6.21: fig06_21.cpp
+// As referências devem ser inicializadas.
+#include <iostream>
+using std::cout;
+using std::endl;
+
+int main()
+{
+    int x = 3;
+    int &y; // Erro: y deve ser inicializado
+
+    cout << "x = " << endl << "y = " << y << endl;
+    return 0; // indica terminação bem-sucedida
+} // fim de main
