05aa4fb1b3a2d6d697809a64b0f199122ba6ad76
Martins <ildefonso.junior@gmail.com>
2026-06-24 07:08:23 -0300

    alteracoes

---
+++ 07/fig07_12.cpp
@@ -0,0 +1,15 @@
+// Figura 7.12: fig07_12.cpp
+// Tratando arrays de caracteres como strings.
+#include <iostream>
+using std::cout;
+using std::cin;
+using std::end;
+
+int main()
+{
+    char string1[ 20 ]; // reserva 20 caracteres
+    char string2[] = "string literal"; // reserva 15 caracteres
+
+    // lê a string fornecida pelo usuário para o array string1
+    cout << "Enter the string"
+}
