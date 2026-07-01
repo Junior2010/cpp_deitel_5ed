e6703120156fe04891b77c8cdb6ba575cbd8e20a
Martins <ildefonso.junior@gmail.com>
2026-07-01 04:14:20 -0300

    fig07_12.cpp

--- 07/fig07_12.cpp
+++ 07/fig07_12.cpp
@@ -3,7 +3,7 @@
 #include <iostream>
 using std::cout;
 using std::cin;
-using std::end;
+using std::endl;
 
 int main()
 {
@@ -11,5 +11,20 @@ int main()
     char string2[] = "string literal"; // reserva 15 caracteres
 
     // lê a string fornecida pelo usuário para o array string1
-    cout << "Enter the string"
-}
+    cout << "Enter the string \"hello there\": ";
+    cin >> string1; // lê “hello” [o espaço termina a entrada]
+
+    // gera a saída de strings
+    cout << "string1 is: " << string1 << "\nstring2 is: " << string2;
+
+    cout << "\nstring1 with spaces beteencharacters is: \n";
+
+    // caracteres de saída até que caractere nulo é alcançado
+    for ( int  i = 0; string1[ i ] != '\0'; i++ )
+        cout << string1[ i ] << ' ';
+
+    cin >> string1;// lê “there”
+    cout << "\nstring1 is: " << string1 << endl;
+
+    return 0; // indica terminação bem-sucedida
+} // fim de main
